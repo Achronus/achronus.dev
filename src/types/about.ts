44ff419555126ca69520fa851d8832dac063ac22
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-05-22 21:10:23 +0100

    Refactor `AboutDetails` data for simplicity
    - Convert 'socials' 'SocialIcons' array
    - Merge 'city' and 'country' into a 'LocationTags' array
    - Refactor 'AboutDetails' -> 'AboutDetailsData'

--- src/types/about.ts
+++ src/types/about.ts
@@ -1,7 +1,13 @@
-
-type SocialIconType = {
+export type SocialIconType = {
   name: string;
   url: URL;
+  viewBox: string;
+  path: string;
+};
+
+export type LocationTagType = {
+  type: string;
+  name: string;
   viewBox: string;
   path: string;
 };
@@ -10,7 +16,6 @@ export type AboutDetailsType = {
   profileImg: string;
   name: string;
   description: string;
-  city: string;
-  country: string;
+  locationTags: LocationTagType[];
   socials: SocialIconType[];
 };
