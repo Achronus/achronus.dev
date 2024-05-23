1521f49950d766ce08853e6823b39f63a590e0a1
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-05-23 12:53:15 +0100

    Add project data array with `Zentra`

---
+++ src/data/projects.ts
@@ -0,0 +1,41 @@
+import { ProjectType } from "@/types/projects";
+import ProfilePic from "@assets/imgs/me.jpg";
+
+import {
+  GitHub,
+  Globe,
+  Pydantic,
+  Python,
+  React,
+  ShadcnUi,
+} from "./icons";
+
+const Zentra: ProjectType = {
+  name: "Zentra",
+  description:
+    "An open-source Python tool that uses Pydantic models to create React components.",
+  logo: ProfilePic,
+  links: [
+    {
+      name: "Website",
+      url: new URL("https://github.com/Achronus/Zentra"),
+      svg: Globe,
+    },
+    {
+      name: "GitHub",
+      url: new URL("https://github.com/Achronus/Zentra"),
+      svg: GitHub,
+    },
+  ],
+  tools: [
+    {
+      name: "Python",
+      svg: Python,
+    },
+    { name: "Pydantic", svg: Pydantic },
+    { name: "React", svg: React },
+    { name: "Shadcn/ui", svg: ShadcnUi },
+  ],
+};
+
+export const ProjectData: ProjectType[] = [Zentra];
