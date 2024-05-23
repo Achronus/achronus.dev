7c87147d6cc85be56e35385e9dcc4f116e89b778
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-05-23 12:50:42 +0100

    Refactor `Icon` with optional `url` and `styles` prop

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,22 +1,40 @@
 import Svg from "@/components/Svg";
+import { cn } from "@/libs/utils";
 import { IconType } from "@/types/global";
 
 export type IconProps = React.SVGAttributes<SVGElement> &
   IconType;
 
-const Icon = ({ name, url, svg, ...props }: IconProps) => {
+const Icon = ({
+  name,
+  url,
+  svg,
+  styles,
+  ...props
+}: IconProps) => {
   return (
     <div
-      className={`${name}-icon hover:bg-base-content/10 hover:opacity-85 p-2 transition-all rounded-md items-center tooltip`}
+      className={cn(
+        `${name}-icon hover:bg-base-content/10 hover:opacity-85 p-2 transition-all rounded-md items-center tooltip`,
+        styles
+      )}
       data-tip={name}
     >
-      <a href={url.href}>
+      {url ? (
+        <a href={url.href}>
+          <Svg
+            path={svg.path}
+            viewBox={svg.viewBox}
+            {...props}
+          />
+        </a>
+      ) : (
         <Svg
           path={svg.path}
           viewBox={svg.viewBox}
           {...props}
         />
-      </a>
+      )}
     </div>
   );
 };
