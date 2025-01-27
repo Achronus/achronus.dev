016e3e02657f68cc42cc82eab32e68f545e1e548
Ryan Partridge <rpartridge101@yahoo.co.uk>
2025-01-27 21:25:01 +0000

    purge(old): Removed old design from branch.
    Removed v2 design from branch and moved specific files to 'frontend' in prep for new one.

--- src/components/sections/index.tsx
+++
@@ -1,6 +0,0 @@
-import AboutHero from "./AboutHero";
-import ContentSection from "./ContentSection";
-import IconCardSection from "./IconCardSection";
-import ProjectsSection from "./ProjectsSection";
-
-export { AboutHero, ContentSection, IconCardSection, ProjectsSection };
--- src/constants/motion.ts
+++
@@ -1,7 +0,0 @@
-export const genericAnimation = {
-  hide: { x: -32, opacity: 0 },
-  show: {
-    x: 0,
-    opacity: 1,
-  },
-};
